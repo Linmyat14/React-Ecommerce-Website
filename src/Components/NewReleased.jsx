9ee855d94e9ae2434addc8f5303136875c4d211e
Linmyat14 <linmyat.n3@gmail.com>
2024-02-03 23:04:55 +0630

    updated

--- src/Components/NewReleased.jsx
+++ src/Components/NewReleased.jsx
@@ -6,16 +6,16 @@ const NewReleased = () => {
         <h5 className="mb-5">Try out our Latset favour here</h5>
         <div className="row">
             <div className="col-6 col-md-4 col-lg-3 mt-lg-0 mt-2">
-                <img className="w-100 rounded-4" src="../../public/images/new-released-1.jpg" alt="" />
+                <img className="w-100 rounded-4" src="../images/new-released-1.jpg" alt="" />
             </div>
             <div className="col-6 col-md-4 col-lg-3 mt-lg-0 mt-2">
-                <img className="w-100 rounded-4" src="../../public/images/new-released-2.jpg" alt="" />
+                <img className="w-100 rounded-4" src="../images/new-released-2.jpg" alt="" />
             </div>
             <div className="col-6 col-md-4 col-lg-3 mt-lg-0 mt-2">
-                <img className="w-100 rounded-4" src="../../public/images/new-released-3.jpg" alt="" />
+                <img className="w-100 rounded-4" src="../images/new-released-3.jpg" alt="" />
             </div>
             <div className="col-6 col-md-4 col-lg-3 mt-lg-0 mt-2">
-                <img className="w-100 rounded-4" src="../../public/images/new-released-4.jpg" alt="" />
+                <img className="w-100 rounded-4" src="../images/new-released-4.jpg" alt="" />
             </div>
         </div>
         <button className="btn px-5 py-2 btn-light rounded-5 mt-4">View More <i className="bi bi-chevron-right"></i></button>
